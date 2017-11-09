5e2491d642556450ae6235a35810c573747e2f70
Alex Woodcroft <newvoxel@gmail.com>
2017-11-09 12:18:44 +1000

    PlayerMovement CameraMovement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	public Vector3 offset;
+	public GameObject target;
+	public float cameraSpeed;
+
+	// Use this for initialization
+	void Start () {
+		offset = new Vector3 (0f, 1.5f, -7f);
+		target = GameObject.FindGameObjectWithTag ("Player");
+		cameraSpeed = 0.1f;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = Vector3.Lerp (transform.position, target.transform.position + offset, cameraSpeed);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody rb = new Rigidbody ();
+	private float playerHeight;
+	public float movementSpeed = 5f;
+	public float jumpHeight = 300f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		playerHeight = GetComponent<CapsuleCollider> ().height;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 targetVelocity = new Vector3 (0, rb.velocity.y, rb.velocity.z);
+
+		if (Input.GetKey ("right") || Input.GetKey("d")) {
+			targetVelocity = new Vector3 (movementSpeed, rb.velocity.y, rb.velocity.z);
+		}
+		if(Input.GetKey("left") || Input.GetKey("a")) {
+			targetVelocity = new Vector3 (-movementSpeed, rb.velocity.y, rb.velocity.z);
+		}
+
+		rb.velocity = targetVelocity;
+
+		RaycastHit hit;
+		if (Physics.Raycast (this.transform.position, Vector3.down, out hit, (playerHeight / 2f) + 0.001f)) {
+			if(Input.GetKeyDown("w") || Input.GetKeyDown("up") || Input.GetKeyDown("space"))
+				rb.AddForce (Vector3.up * jumpHeight);
+		}
+	}
+}
